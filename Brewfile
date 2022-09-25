5305680478c5c04cbbd600f7a0e50f2be631c422
Thiago Colares <thicolares@gmail.com>
2022-09-25 11:45:15 -0400

    First commit

---
+++ Brewfile
@@ -0,0 +1,61 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# Utilities
+cask "tiles"
+cask "firefox"
+cask "google-chrome"
+cask "grammarly"
+cask "iterm2"
+cask "slack"
+cask "telegram"
+cask "whatsapp"
+cask "transmission"
+cask "spotify"
+cask "flycut"
+brew "jq"
+brew "nmap"
+brew "openssl"
+brew "watch"
+cask "docker"
+brew "docker-compose"
+
+brew "zsh"
+brew "zsh-completions"
+
+# Dev
+cask "postman"
+brew "git"
+brew "nvm"
+brew "node"
+brew "tmux"
+brew "vim"
+brew "zlib"
+cask "sublime-text"
+cask "macdown"
+cask "sequel-pro"
+casl "visual-studio-code"
+
+# Util
+cask "appcleaner"
+cask "coconutbattery"
+cask "the-unarchiver"
+cask "cheatsheet"
+
+# System
+brew "mas"
+
+# Network
+brew "htop"
+brew "nmap"
+brew "mtr"
+brew "wget"
+
+# Communication
+cask "slack"
+cask "skype"
+
+# App
+mas "XCode", id: 497799835
---
+++ init.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+echo "Installing Homebrew"
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+brew update
+echo "Install Homebrew Packages"
+brew tap homebrew/bundle
+brew bundle
+
+echo "Install XCode CLI Tool"
+xcode-select --install
+
+echo "Installing Oh My Zsh"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+echo "source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+sed -io 's/^plugins=.*/plugins=(autojump git brew common-aliases zsh-autosuggestions copydir copyfile encode64 node osx sublime tmux xcode pod docker git-extras git-prompt)/' ~/.zshrc
+sed -io 's/^ZSH_THEME.*/ZSH_THEME="dpoggi"/' ~/.zshrc
+
+echo "Installing Tmux Settings"
+git clone https://github.com/gpakosz/.tmux.git ~/.tmux
+ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+cp ~/.tmux/.tmux.conf.local ~/
+
+echo "Install VIM settings"
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+echo "Configrating Git"
+cp gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
+git config --global merge.tool diffmerge
+git config --global merge.conflictstyle diff3
+git config --global mergetool.prompt false
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global core.editor $(which vim)
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.tree "log --graph --full-history --all --color --date=short --pretty=format:'%Cred%x09%h %Creset%ad%Cblue%d %Creset %s %C(bold)(%an)%Creset'"
+
+
+echo "Enabling Services"
+open /Applications/Tiles.app
